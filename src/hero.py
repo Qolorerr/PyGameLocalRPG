a91fda851ab01b7351337ab2489bcccb2bc422c3
Danila <kdv0807@gmail.com>
2019-11-30 16:12:29 +0700

    hero control fixed and add step count

--- src/hero.py
+++ src/hero.py
@@ -17,33 +17,50 @@ class Hero(Essence):
         self.mainHero = mainHero
         self.ablities = dict()
         self.attack_mode = False
+        self.steps = move_distance
+
+    def do_step(self):
+        if self.steps == 0:
+            return False
+        self.steps -= 1
+        return True
+
+    def attack(self, other_essence, type_of_attack=3):
+        if self.steps > 0:
+            super().attack(other_essence, type_of_attack)
+            self.do_step()
+
+    def move(self, new_location, map):
+        if self.steps > 0:
+            super().move(new_location, map)
+            self.do_step()
 
     def get_event(self, keydown_unicode, gameMap, screen):
-        if keydown_unicode == 'w':
+        if keydown_unicode in ['w', 'ц']:
             new_location = (self.location[0], self.location[1] - 1)
             if self.move(new_location, gameMap):
                 camera.append(camera[1] + (gameMap.cell_size))
                 del(camera[1])
-        elif keydown_unicode == 's':
+        elif keydown_unicode in ['s', 'ы']:
             new_location = (self.location[0], self.location[1] + 1)
             if self.move(new_location, gameMap):
                 camera.append(camera[1] - (gameMap.cell_size))
                 del (camera[1])
-        elif keydown_unicode == 'a':
+        elif keydown_unicode in ['a', 'ф']:
             new_location = (self.location[0] - 1, self.location[1])
             if self.move(new_location, gameMap):
                 camera.append(camera[0] + (gameMap.cell_size))
                 camera.append(camera[1])
                 del(camera[0])
                 del(camera[0])
-        elif keydown_unicode == 'd':
+        elif keydown_unicode in ['d', 'в']:
             new_location = (self.location[0] + 1, self.location[1])
             if self.move(new_location, gameMap):
                 camera.append(camera[0] - (gameMap.cell_size))
                 camera.append(camera[1])
                 del (camera[0])
                 del (camera[0])
-        elif keydown_unicode == 'q':
+        elif keydown_unicode in ['q', 'й']:
             self.attack_mode = not self.attack_mode
 
     def render_move_zone(self, screen, map):
@@ -51,9 +68,9 @@ class Hero(Essence):
         y = map.top + self.location[1] * map.cell_size
         if self.mainHero:
             pygame.draw.rect(screen, pygame.Color("red"), (x + camera[0], y + camera[1], map.cell_size, map.cell_size), 2)
-            for i in range(-self.move_distance, self.move_distance + 1):
-                for j in range(-self.move_distance, self.move_distance + 1):
-                    if abs(i) + abs(j) > self.move_distance:
+            for i in range(-self.steps, self.steps + 1):
+                for j in range(-self.steps, self.steps + 1):
+                    if abs(i) + abs(j) > self.steps:
                         continue
                     x = map.left + (self.location[0] + i) * map.cell_size - map.indent
                     y = map.top + (self.location[1] + j) * map.cell_size - map.indent
@@ -72,4 +89,5 @@ class Hero(Essence):
     def render(self, screen, map):
         self.render_move_zone(screen, map)
         super().render(screen, map)
-        self.render_can_attack(screen, map)
+        if self.steps > 0:
+            self.render_can_attack(screen, map)
