f7125921a0f98a4af383eceebbb49c56ca08ecf4
KurlykovDanila <56872251+kurlykovdanila@users.noreply.github.com>
2020-01-20 21:28:36 +0700

    debugs

--- src/death.py
+++ src/death.py
@@ -17,3 +17,4 @@ def death(screen, resolution, state):
         for event in pygame.event.get():
             if event.type == pygame.QUIT or event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
                 pygame.quit()
+                exit()
